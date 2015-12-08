771b2baa160a3cb4d96de3a3cf33cc2087a9a81e
Surawut Sodasak <giisurawut@gmail.com>
2015-12-09 01:32:29 +0700

    login

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>MITRPHOL COMPETENCY LANE</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="./css/bootstrap.min.css" rel="stylesheet">
+		<link href="./css/login-styles.css" rel="stylesheet">
+	</head>
+	<body>
+<!--login modal-->
+<div id="loginModal" class="modal show" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+  <div class="modal-content">
+      <div class="modal-header">
+					<h2>MITR PHOL</h2>
+					<h3>COMPETENCY LANE</h3>
+      </div>
+      <div class="modal-body">
+          <form class="form col-md-12 center-block">
+            <div class="form-group">
+              <input type="text" class="form-control input-lg" placeholder="Email">
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control input-lg" placeholder="Password">
+            </div>
+            <div class="form-group">
+              <button class="btn btn-primary btn-lg btn-block">Sign In</button>
+            </div>
+          </form>
+      </div>
+      <div class="modal-footer"></div>
+  </div>
+  </div>
+</div>
+	<!-- script references -->
+		<script src="./js/jquery-1.11.1.min.js"></script>
+		<script src="./js/bootstrap.min.js"></script>
+	</body>
+</html>
