c2305818e73f6ab13ec2187a220a9d5852b1da4c
Surawut Sodasak <giisurawut@gmail.com>
2015-12-09 05:04:19 +0700

    menu admin

---
+++ includes/menu-admin.php
@@ -0,0 +1,20 @@
+<div class="col-sm-3 col-md-2 sidebar">
+  <div class="top-navigation">
+    <div class="t-menu">MENU</div>
+    <div class="t-img">
+      <img src="images/lines.png" alt="" />
+    </div>
+    <div class="clearfix"> </div>
+  </div>
+
+  <div class="drop-navigation">
+    <ul class="nav nav-sidebar">
+      <li class="active"><a href="#" class="home-icon"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Dashboard</a></li>
+      <li><a href="#" class="home-icon"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Category</a></li>
+      <li><a href="#" class="home-icon"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Content</a></li>
+      <li><a href="#" class="home-icon"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Examination</a></li>
+      <li><a href="#" class="home-icon"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Configuration</a></li>
+    </ul>
+  </div>
+
+</div>
